ef7eb6c3432f888a9cd32be402711d1ba8848e05
Saad05-dev <bougrasaad@gmail.com>
2025-10-24 12:32:13 +0100

    Registration and Creating Account Complete

--- auth/register_process.php
+++ auth/register_process.php
@@ -1,5 +1,18 @@
 <?php
 
+//handling errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+//Getting the values of necessary fields from the register form
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$countryCode = $_POST['country-code'];
+$phone_number = $_POST['phone'];
+$phone = $countryCode . $phone_number; 
+
 //loading config
 $config = require_once '../config/database.php';
 
@@ -21,5 +34,20 @@ $conn->set_charset($config['charset']);
 
 //Adding new user
 
+$stmt = $conn->prepare("INSERT INTO customers (email, password, first_name, last_name, phone) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sssss",$email,$password,$first_name,$last_name,$phone);
+
+//Excecution
+if($stmt->execute())
+{
+    header("Location: login.php");
+    exit;
+}
+else
+{
+    echo "Error:" . $stmt->error;
+}
+$stmt->close();
+$conn->close();
 
 ?>
