f49186e03e5f233eba57e444fae22be4bb04f0b3
Saad05-dev <bougrasaad@gmail.com>
2025-11-04 14:33:41 +0100

    Starting Dashboard page

---
+++ user/dashboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../assets/css/dashboard.css">
+</head>
+<body>
+    <!-- Navigation Bar-->
+    <nav class="nav-bar">
+        <div class="logo"><a href="#"><img src="../assets/images/MV5BYmE2MGQ5NTMtMDhhNi00MDhjLTgwOTUtNWI2M2M0ZDQyMDI4XkEyXkFqcGc@.webp" alt=""></a></div>
+        <div class="nav">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Cart</a></li>
+                <li><div class="login-signup"><a href="../auth/login.php">Login/Signup</a></div></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Body -->
+    <section class="first-section">
+        <div class="box">
+            
+        </div>
+    </section>
+</body>
+</html>
